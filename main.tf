4b50b325f0d5e3ccbae0e1240a8020d4be645d64
uires <uiresdeivide1@gmail.com>
2023-08-26 17:18:08 -0300

    base config

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0f8e81a3da6e2510a"
+  instance_type = "t2.micro"
+  key_name      = "default-california"
+  tags = {
+    Name = "AppMasc"
+  }
+}
