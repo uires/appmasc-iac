4e216d2c52b230be4225ebfe35fcd091dedbd43f
uires <uiresdeivide1@gmail.com>
2024-01-16 22:09:20 -0300

    configured load balancer

--- infra/main.tf
+++ infra/main.tf
@@ -30,13 +30,45 @@ resource "aws_key_pair" "sshKey" {
 }
 
 resource "aws_autoscaling_group" "autoscaling_group" {
-  availability_zones = ["${var.region_aws}b"]
+  availability_zones = ["${var.region_aws}b", "${var.region_aws}c"]
   name               = var.autoscaling_group_name
   max_size           = var.autoscaling_group_max_size
   min_size           = var.autoscaling_group_min_size
+  target_group_arns  = [aws_lb.load_balancer.arn]
   launch_template {
     id      = aws_launch_template.machine.id
     version = "$Latest"
   }
+}
 
+resource "aws_default_subnet" "subnet_1" {
+  availability_zone = "${var.region_aws}b"
+}
+
+resource "aws_default_subnet" "subnet_2" {
+  availability_zone = "${var.region_aws}c"
+}
+
+resource "aws_lb" "load_balancer" {
+  internal = false
+  subnets  = [aws_default_subnet.subnet_1.id, aws_default_subnet.subnet_2.id]
+}
+
+resource "aws_lb_target_group" "target_load_balancer" {
+  name     = "target"
+  port     = "8000"
+  protocol = "HTTP"
+  vpc_id   = aws_default_vpc.default.id
+}
+
+resource "aws_default_vpc" "default" {}
+
+resource "aws_lb_listener" "load_balancer_entry_point" {
+  load_balancer_arn = aws_lb.load_balancer.arn
+  port              = "8000"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_load_balancer.arn
+  }
 }
